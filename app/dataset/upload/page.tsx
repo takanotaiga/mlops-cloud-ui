f4325b5fb09c947c4837c72bb1ce2e404ca9e8c6
Taiga Takano <ttttghghnb554z@outlook.jp>
2025-08-05 14:36:39 +0900

    refactor: replace file upload component with framework selection dropdown

--- app/dataset/upload/page.tsx
+++ app/dataset/upload/page.tsx
@@ -1,47 +1,67 @@
+"use client"
+
 import {
   Text,
+  Box,
   HStack,
   VStack,
   Heading,
-  Wrap,
   Button,
   Input,
-  InputGroup,
-  Spacer
+  Select,
+  createListCollection,
+  Portal
 } from "@chakra-ui/react"
 
-import ImageCard from "@/components/image-card"
-import { BsPlusCircleFill } from "react-icons/bs";
-import { LuSearch } from "react-icons/lu"
-
-import { Box, FileUpload, Icon } from "@chakra-ui/react"
+import { FileUpload, Icon } from "@chakra-ui/react"
 import { LuUpload } from "react-icons/lu"
+
+import { LuCloudUpload } from "react-icons/lu";
 
 const Demo = () => {
   return (
-    <FileUpload.Root maxW="xl" alignItems="stretch" maxFiles={1024}>
-      <FileUpload.HiddenInput />
-      <FileUpload.Dropzone>
-        <Icon size="md" color="fg.muted">
-          <LuUpload />
-        </Icon>
-        <FileUpload.DropzoneContent>
-          <Box>Drag and drop files here</Box>
-          <Box color="fg.muted">.png, .jpg up to 5MB</Box>
-        </FileUpload.DropzoneContent>
-      </FileUpload.Dropzone>
-      <FileUpload.List />
-    </FileUpload.Root>
+    <Select.Root collection={frameworks} size="sm" width="320px">
+      <Select.HiddenSelect />
+      <Select.Control>
+        <Select.Trigger>
+          <Select.ValueText placeholder="Select framework" />
+        </Select.Trigger>
+        <Select.IndicatorGroup>
+          <Select.Indicator />
+        </Select.IndicatorGroup>
+      </Select.Control>
+      <Portal>
+        <Select.Positioner>
+          <Select.Content>
+            {frameworks.items.map((framework) => (
+              <Select.Item item={framework} key={framework.value}>
+                {framework.label}
+                <Select.ItemIndicator />
+              </Select.Item>
+            ))}
+          </Select.Content>
+        </Select.Positioner>
+      </Portal>
+    </Select.Root>
   )
 }
 
+const frameworks = createListCollection({
+  items: [
+    { label: "React.js", value: "react" },
+    { label: "Vue.js", value: "vue" },
+    { label: "Angular", value: "angular" },
+    { label: "Svelte", value: "svelte" },
+  ],
+})
 
-export default async function Page() {
+
+export default function Page() {
   return (
 
     <HStack justify="center">
       <VStack w="70%" >
-        <HStack w="95%" justify="space-between" pb="40px" pt="30px">
+        <HStack w="95%" justify="space-between" pt="40px">
           <Box alignSelf="flex-start" ml="30px">
             <HStack alignSelf="flex-start">
               <Heading size="2xl" >Upload</Heading>
@@ -49,33 +69,55 @@ export default async function Page() {
           </Box>
         </HStack>
 
-        <HStack w="95%" justify="space-between" pb="40px" pt="30px">
+        <HStack w="95%" justify="space-between" pb="40px" pt="40px">
           <Box alignSelf="flex-start" ml="30px">
-            <HStack alignSelf="flex-start">
+            <HStack alignSelf="flex-start" pb="15px">
               <Heading size="md" >Infomation</Heading>
             </HStack>
-
-            <HStack alignSelf="flex-start">
-              <Text w="100px">Image</Text>
+            <HStack alignSelf="flex-start" pb="10px">
+              <Text w="100px" ml="30px">Image</Text>
               <Text minW="60px" textAlign="right">1,021</Text>
             </HStack>
-            <HStack alignSelf="flex-start">
-              <Text w="100px">Video</Text>
-              <Text minW="60px" textAlign="right">10</Text>
+            <HStack alignSelf="flex-start" pb="10px">
+              <Text w="100px" ml="30px">Video</Text>
+              <Text minW="60px" textAlign="right">0</Text>
             </HStack>
 
-            <HStack alignSelf="flex-start">
+            <HStack alignSelf="flex-start" pt="30px" pb="15px">
               <Heading size="md" >Congiguration</Heading>
             </HStack>
 
-            <HStack alignSelf="flex-start">
-              <Text w="100px">Image</Text>
-              <Text minW="60px" textAlign="right">1,021</Text>
+            <HStack alignSelf="flex-start" pb="30px">
+              <Text w="200px" ml="30px">Dataset title</Text>
+              <Input ml="30px" placeholder="Write here" variant="flushed" />
             </HStack>
-            <HStack alignSelf="flex-start">
-              <Text w="100px">Video</Text>
-              <Text minW="60px" textAlign="right">10</Text>
+            <HStack alignSelf="flex-start" pb="30px">
+              <Text w="200px" ml="30px">Encode Mode</Text>
+              <Demo />
             </HStack>
+          </Box>
+          <Box alignSelf="flex-start" ml="30px">
+            <FileUpload.Root maxW="xl" alignItems="stretch" maxFiles={10}>
+              <FileUpload.HiddenInput />
+              <FileUpload.Dropzone>
+                <Icon size="md" color="fg.muted">
+                  <LuUpload />
+                </Icon>
+                <FileUpload.DropzoneContent>
+                  <Box>Drag and drop files here</Box>
+                  <Box color="fg.muted">.png, .jpg up to 5MB</Box>
+                </FileUpload.DropzoneContent>
+              </FileUpload.Dropzone>
+            </FileUpload.Root>
+          </Box>
+        </HStack>
+
+        <HStack w="95%" justify="space-between" pb="40px" pt="40px">
+          <Box alignSelf="flex-start" ml="30px">
+            <Button variant="subtle" rounded="full" w="200px">
+              <LuCloudUpload />
+              Upload to cloud
+            </Button>
           </Box>
         </HStack>
       </VStack>
